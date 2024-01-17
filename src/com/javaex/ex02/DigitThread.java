0a343ef7d7888dcefc1b92953a6ff1afafeb756a
aassdd236 <aassdd236@naver.com>
2024-01-17 15:07:04 +0900

    상속 Thread

---
+++ src/com/javaex/ex02/DigitThread.java
@@ -0,0 +1,15 @@
+package com.javaex.ex02;
+
+public class DigitThread extends Thread {
+	public void run() {
+		for(int i=0; i<30; i++) {
+			System.out.println(i+1);
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+
+		}
+	}
+}
